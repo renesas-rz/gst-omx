3efa4a54ac58af51cf611803e1c21cef50aff0e7
trungvanle <trung.le.xk@renesas.com>
2024-08-27 15:28:37 +0700

    omxvideoenc: Support hack renesas-encmc-stride-align
    This hack support for Renesas OMXIL encoder library
    Renesas encoder MC requires nStride should align for 32 in case semi-planar
    (NV12) format and align for 64 in case planar (I420) format.
    Declare this hack in gstomx.conf to use it.
    For example:
    [omxh264enc]
    ...
    hacks=no-disable-outport;renesas-encmc-stride-align
    
    refs #138391
    Signed-off-by: Anh Ha <anh.ha.wz@rvc.renesas.com>
    
    porting from 1.12.2 to 1.14.2
    Signed-off-by: Shinya Saito <ssaito@igel.co.jp>
    
    Porting from 1.18.5 to 1.22.11
    Signed-off-by: trungvanle <trung.le.xk@renesas.com>

--- omx/gstomx.c
+++ omx/gstomx.c
@@ -3843,6 +3843,8 @@ gst_omx_parse_hacks (gchar ** hacks)
       hacks_flags |= GST_OMX_HACK_USE_NO_COPY_MODE_AS_DEFAULT;
     else if (g_str_equal (*hacks, "default-pix-aspect-ratio"))
       hacks_flags |= GST_OMX_HACK_DEFAULT_PIXEL_ASPECT_RATIO;
+    else if (g_str_equal (*hacks, "renesas-encmc-stride-align"))
+      hacks_flags |= GST_OMX_HACK_RENESAS_ENCMC_STRIDE_ALIGN;
     else
       GST_WARNING ("Unknown hack: %s", *hacks);
     hacks++;
